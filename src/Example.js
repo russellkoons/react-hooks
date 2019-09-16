69b6c4c6eb20005e610b7d710e0b74c046840d9d
russellkoons <rkoons@gmail.com>
2019-09-16 17:24:09 -0400

    comments

--- src/Example.js
+++ src/Example.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export function Example() {
+  // State Hook
   const [count, setCount] = useState(0);
+
+  // Effect Hook
+  // Changes the title as the DOM is updated
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
 
   return(
     <div>
@@ -13,8 +20,8 @@ export function Example() {
 
 // You can also use multiple state hooks in one component
 
-function ManyStates() {
-  const [age, setAge] = useState(42);
-  const [fruit, setFruit] = useState('strawberry');
-  const [todos, setTodos] = useState([{ text: 'Learn React Hooks!'}]);
-}
+// function ManyStates() {
+//   const [age, setAge] = useState(42);
+//   const [fruit, setFruit] = useState('strawberry');
+//   const [todos, setTodos] = useState([{ text: 'Learn React Hooks!'}]);
+// }
